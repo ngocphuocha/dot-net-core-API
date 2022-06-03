bd30c0b2218d1c08f514cf5e0e12fddde2258979
phuoc <phuoc@DESKTOP-EIVJR63>
2022-06-03 08:24:07 +0700

    Add project files.

---
+++ TodoApp/Controllers/TodoController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApp.Data;
+using TodoApp.Models;
+
+namespace TodoApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        // khi nao dung formBody vi em thay trong create voi update ko them dung ma cai register thi dung?
+
+        private readonly ApiDbContext _context;
+
+        public TodoController(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public ActionResult GetItems()
+        {
+            var items = _context.Items.ToList();
+            return Ok(items);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetItem(int id)
+        {
+            var item = await _context.Items.FirstOrDefaultAsync(z => z.Id == id);
+
+            if (item == null)
+                return NotFound();
+
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateItem(ItemData data)
+        {
+            if (ModelState.IsValid)
+            {
+                await _context.Items.AddAsync(data);
+                await _context.SaveChangesAsync();
+
+                return CreatedAtAction("GetItem", new { data.Id }, data);
+            }
+
+            return new JsonResult("Somethign Went wrong") { StatusCode = 500 };
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateItem(int id, ItemData item)
+        {
+            // check if the id of formdata not equal ItemData Id attribute
+            if (id != item.Id)
+                return BadRequest();
+
+            var existItem = await _context.Items.FirstOrDefaultAsync(z => z.Id == id);
+
+            if (existItem == null)
+                return NotFound();
+
+            existItem.Title = item.Title;
+            existItem.Details = item.Details;
+            existItem.Done = item.Done;
+
+            await _context.SaveChangesAsync();
+
+            // Following up the REST standart on update we need to return NoContent
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteItem(int id)
+        {
+            var existItem = await _context.Items.FirstOrDefaultAsync(z => z.Id == id);
+
+            if (existItem == null)
+                return NotFound();
+
+            _context.Items.Remove(existItem);
+            await _context.SaveChangesAsync();
+
+            return Ok(existItem);
+        }
+    }
+}
---
+++ TodoApp/Models/ItemData.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoApp.Models
+{
+    public class ItemData
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Details { get; set; }
+        [Required]
+        public bool Done { get; set; }
+        // bao thay fix o day
+    }
+}
